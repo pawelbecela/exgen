3c52fdf7016b7b9a1fb06d748398e545828f0eb6
Pawel Becela <pawbecela@gmail.com>
2023-07-08 10:10:49 +0200

    removed shit from db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_05_085417) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_08_080841) do
   create_table "exercises", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -19,7 +19,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_05_085417) do
 
   create_table "notes", force: :cascade do |t|
     t.string "name"
-    t.integer "position"
     t.integer "exercise_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
